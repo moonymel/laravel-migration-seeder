0d776b27250f20d61d4f3c171ab8810da08292a8
moonymel <musumeci.me@gmail.com>
2024-02-15 16:21:56 +0100

    [UPDATE] created migration for trains

---
+++ database/migrations/2024_02_15_145727_trains.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->timestamp('departure_time');
+            $table->timestamp('arrival_time');
+            $table->mediumInteger('code');
+            $table->tinyInteger('n_carriages');
+            $table->tinyInteger('in_time')->unsigned()->default(1);
+            $table->tinyInteger('cancelled')->unsigned()->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
